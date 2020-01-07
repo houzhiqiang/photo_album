19c97c3ad84fd7b94b9df48ccfb28e1c3c5d49f9
熊孩子强 <xhzq@ixhzq.com>
2020-01-07 18:49:13 +0800

    change gunicorn.conf.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from pathlib import Path
 
 
@@ -9,7 +10,12 @@ class Config:
     GUNICORN_RELOAD = True
     USERNAME = "username"
     PASSWORD = "password"
-    BASE_PATH = Path(__file__).parent # f"{'/'.join(__file__.split('/')[0: -1])}"
+    BASE_PATH = Path(__file__).parent
     PHOTO_FILE_PATH = BASE_PATH.joinpath('static/photo')
     STATIC_URL_PATH = '/static'
     SECRET_KEY = "adafaferqe-faed341adsfJdFds你哈发！！！"
+    WORKERS = multiprocessing.cpu_count() * 2 + 1
+    LOGLEVEL = 'info'
+    ACCESS_LOG_FORMAT = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'
+    ACCESSLOG = "/home/ixhzq/web/photo_album/log/gunicorn/access.log"
+    ERRORLOG = "/home/ixhzq/web/photo_album/log/gunicorn/error.log"
--- photo_album.gunicorn.conf.py
+++ photo_album.gunicorn.conf.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import multiprocessing
-
 try:
     from local_config import Config
 except:
@@ -10,18 +8,18 @@ except:
 
 # bind = 'unix:/tmp/gunicorn.sock'
 bind = f'{Config.HOST}:{Config.PORT}'
-workers = 2 # multiprocessing.cpu_count() * 2 + 1
+workers = Config.WORKERS
 worker_class = 'gevent'
 threads = 2
 max_requests = 10000
-# chdir = '/home/ixhzq/web/photo_album'
+chdir = Config.BASE_PATH
 daemon = False
 pidfile = '/tmp/gunicorn.pid'
 proc_name = 'gunicorn'
 
-loglevel = 'info'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Real-IP}i)s"'
-accesslog = "/home/ixhzq/web/photo_album/log/gunicorn/access.log"
-errorlog = "/home/ixhzq/web/photo_album/log/gunicorn/error.log"
+loglevel = Config.LOGLEVEL
+access_log_format = Config.ACCESS_LOG_FORMAT
+accesslog = Config.ACCESSLOG
+errorlog = Config.ERRORLOG
 
 reload = Config.GUNICORN_RELOAD
